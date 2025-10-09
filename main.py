5baec8923e60e2ccf3e390618d376989073301e5
odaymare02 <mareoday123@gmail.com>
2025-10-09 07:06:35 +0300

    fix: use Render dynamic port

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from api import router as api_router
-
+import uvicorn
+import os
 app = FastAPI(title="RAG API")
 
 # إضافة الراوتر
 app.include_router(api_router, prefix="/api")
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
